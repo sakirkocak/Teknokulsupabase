20390b46756e0786b7b7d60e619dedabcc649ec7
Sakir Kocak <sakirkocak@MacBookAir.hgw.local>
2025-12-21 21:11:41 +0300

    feat: Yeni Google Ads etiketi eklendi (AW-17821953417)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
       <head>
         {/* Google Ads Tag (gtag.js) */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-16918605673"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17821953417"
           strategy="afterInteractive"
         />
         <Script id="google-ads" strategy="afterInteractive">
@@ -45,6 +45,8 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
+            
+            gtag('config', 'AW-17821953417');
             gtag('config', 'AW-16918605673');
           `}
         </Script>
